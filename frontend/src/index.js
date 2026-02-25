588724d00cbde80c06770b5d0802bf06653005c5
Medcures <medcures15@gmail.com>
2026-02-25 23:26:58 +0530

    Add service worker cleanup and anti-caching measures

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,6 +3,15 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
+// Unregister all service workers to prevent caching
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.getRegistrations().then((registrations) => {
+    for (let registration of registrations) {
+      registration.unregister();
+    }
+  });
+}
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
